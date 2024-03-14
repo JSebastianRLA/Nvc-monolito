33b7382f2633d3971133105d1e5c8d7912ea86f0
JSebastianRLA <joan.rojas@ngeek.co>
2024-03-14 14:28:31 -0500

    cambios suptitulo login

--- general/logon_ok.php
+++ general/logon_ok.php
@@ -213,7 +213,7 @@ if (!empty($news)) {
     foreach ($news as $article) {
         $default = false;
         if ($article['text'] == '&amp;lt;p&#x20;style=&quot;text-align:&#x20;center;&#x20;font-size:&#x20;13px;&quot;&amp;gt;Hello,&#x20;congratulations,&#x20;if&#x20;you&apos;ve&#x20;arrived&#x20;here&#x20;you&#x20;already&#x20;have&#x20;an&#x20;operational&#x20;monitoring&#x20;console.&#x20;Remember&#x20;that&#x20;our&#x20;forums&#x20;and&#x20;online&#x20;documentation&#x20;are&#x20;available&#x20;24x7&#x20;to&#x20;get&#x20;you&#x20;out&#x20;of&#x20;any&#x20;trouble.&#x20;You&#x20;can&#x20;replace&#x20;this&#x20;message&#x20;with&#x20;a&#x20;personalized&#x20;one&#x20;at&#x20;Admin&#x20;tools&#x20;-&amp;amp;gt;&#x20;Site&#x20;news.&amp;lt;/p&amp;gt;&#x20;') {
-            $article['subject'] = __('Welcome to Pandora FMS Console');
+            $article['subject'] = __('Welcome to NVC Console');
             $default = true;
         }
 
